f927b9e7fc406bd0cc7a88890d110fe0fefba78d
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-15 19:53:01 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  badges = Array.new
+  for name in array
+    badges << badge_maker(name)
+  end
+  badges
+end
+
+def assign_rooms(array)
+  assignments = Array.new
+  for room in 0...array.length
+    assignments << "Hello, #{array[room]}! You'll be assigned to room #{room+1}!"
+  end
+  assignments
+  
+end
+
+def printer(array)
+  for badge in batch_badge_creator(array)
+    puts badge
+  end
+  for assignment in assign_rooms(array)
+    puts assignment
+  end
+end
