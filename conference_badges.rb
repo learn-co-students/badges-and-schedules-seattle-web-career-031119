54a91b95f2c04b3b91d9a7e1e71996a9acec9a4b
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-20 21:59:21 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.collect { |x|
+    badge_maker(x)
+  }
+end
+
+def assign_rooms(array)
+  array.collect { |x|
+    "Hello, #{x}! You'll be assigned to room #{(array.index(x) + 1)}!"
+  }
+end
+
+##
+#def assign_rooms(array)
+# array.each_with_index { |x, i|
+#   "Hello, #{x}! You'll be assigned to room #{(i + 1)}!"
+# }
+#end
+
+def printer(array)
+  batch_badge_creator(array).each { |x|
+    puts x 
+  }
+  assign_rooms(array).each { |x|
+    puts x 
+  }
+end
