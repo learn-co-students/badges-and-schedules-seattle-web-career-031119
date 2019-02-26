9fc700b91cfa1bcd86cf105fe2371d712db37cd5
github username <github email address>
2019-02-26 09:00:42 -0800

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,18 @@ def assign_rooms(list)
   count = 0
   room_assignments = Array.new
   while count < list.length
-    room_assignments << "Hello #{list[count]}! You'll be assigned to room #{count + 1}"
+    room_assignments << "Hello, #{list[count]}! You'll be assigned to room #{count + 1}!"
+    count += 1
   end
   room_assignments
+end
+
+def printer(attendes)
+  list_of_assignments = assign_rooms(attendes)
+  count = 0
+  while count < list_of_assignments.length
+    puts list_of_assignments[count]
+    count += 1
+  end
+
 end
